b76fe596f2fb29561e067f2c9ba2b03da9f06002
Bryan <bryan@orteo.co>
2021-03-21 09:10:33 +0300

    enable playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StarWarsModule } from './star-wars/star-wars.module';
 
 @Module({
-  imports: [StarWarsModule,
-    // GraphQLModule.forRoot({})
+  imports: [
+    StarWarsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
