ac9de071bdc5e20ecb4f8607ae7cdbeb1fa7f6af
Bryan <bryan@orteo.co>
2021-03-22 22:46:59 +0300

    further caching with redis disclaimer

--- src/common/enviroment-vars.ts
+++ src/common/enviroment-vars.ts
@@ -2,8 +2,9 @@ export const swapi = {
   main: 'https://swapi.dev/api',
 };
 
+/* Caching works fastest with a local redis instance rather than a cloud/remote one
+   In the case of a code sandbox, I have to use a remote set up as the sandbox does not avail one locally*/
 export const redisConfigs = {
-  // Caching works fastest with a local redis instance rather than a cloud/remote one
   // host: '127.0.0.1',  --local
   host: '3.139.2.178',
   port: '6379',
