e4f2da70534adf177c2fc3e9aefd27999fc8e614
Bryan <bryan@orteo.co>
2021-03-22 22:44:45 +0300

    caching disclaimer

--- src/common/enviroment-vars.ts
+++ src/common/enviroment-vars.ts
@@ -3,6 +3,7 @@ export const swapi = {
 };
 
 export const redisConfigs = {
+  // Caching works fastest with a local redis instance rather than a cloud/remote one
   // host: '127.0.0.1',  --local
   host: '3.139.2.178',
   port: '6379',
